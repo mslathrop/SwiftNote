70e9b2ff665c4e663484c60868cfbf6510b31187
Matt Lathrop <mslathrop@gmail.com>
2014-06-03 19:27:49 -0400

    Created classes for notes table and navigation controller

---
+++ SwiftNote/Note.swift
@@ -0,0 +1,18 @@
+//
+//  Note.swift
+//  SwiftNote
+//
+//  Created by Matthew Lathrop on 6/3/14.
+//  Copyright (c) 2014 Matt Lathrop. All rights reserved.
+//
+
+import CoreData
+import UIKit
+
+class Note: NSManagedObject {
+    @NSManaged var body: NSString
+    @NSManaged var createdAt: NSDate
+    @NSManaged var entityId: NSString
+    @NSManaged var modifiedAt: NSDate
+    @NSManaged var title: NSString
+}
---
+++ SwiftNote/SwiftNoteNavigationController.swift
@@ -0,0 +1,39 @@
+//
+//  SwiftNoteNavigationController.swift
+//  SwiftNote
+//
+//  Created by Matthew Lathrop on 6/3/14.
+//  Copyright (c) 2014 Matt Lathrop. All rights reserved.
+//
+
+import UIKit
+
+class SwiftNoteNavigationController: UINavigationController {
+
+    init(coder aDecoder: NSCoder!) {
+        super.init(coder: aDecoder)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
